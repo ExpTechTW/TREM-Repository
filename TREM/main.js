c8f8064999be951430adb78f324296dd8c6031a8
whes1015 <whes1015@gmail.com>
2022-07-14 16:52:05 +0800

    fix: Name

--- TREM/main.js
+++ TREM/main.js
@@ -24,7 +24,7 @@ app.setLoginItemSettings({
 
 function createWindow() {
 	MainWindow = new BrowserWindow({
-		title          : "TREM | 台灣實時地震監測",
+		title          : "TREM | 台灣即時地震監測",
 		width          : 1280,
 		height         : 720,
 		resizable      : false,
@@ -46,7 +46,7 @@ function createWindow() {
 	pushReceiver.setup(MainWindow.webContents);
 
 	if (process.platform === "win32")
-		app.setAppUserModelId("TREM | 台灣實時地震監測");
+		app.setAppUserModelId("TREM | 台灣即時地震監測");
 
 	MainWindow.on("close", (event) => {
 		if (app.quitting)
@@ -129,7 +129,7 @@ else {
 				},
 			},
 		]);
-		tray.setToolTip("TREM | 台灣實時地震監測");
+		tray.setToolTip("TREM | 台灣即時地震監測");
 		tray.setContextMenu(contextMenu);
 		tray.setIgnoreDoubleClickEvents(true);
 		tray.on("click", (e) => {
