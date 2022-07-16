da4ef476d84235cbe62532902c97b37245364971
whes1015 <whes1015@gmail.com>
2022-07-16 15:09:01 +0800

    feat: 地圖點擊歸位

--- TREM/js/earthquake.js
+++ TREM/js/earthquake.js
@@ -167,9 +167,8 @@ function init() {
 			ReportMarkID = null;
 			for (let index = 0; index < MarkList.length; index++)
 				map.removeLayer(MarkList[index]);
-
-			focus();
 		}
+		focus();
 	});
 
 	mapLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw", {
