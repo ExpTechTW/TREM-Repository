3600e35c00b10a227ba79178f3e70b0d97030a55
kamiya10 <58339640+kamiya10@users.noreply.github.com>
2022-07-17 04:00:23 +0800

    feat: 圓角設定

--- TREM/js/setting.js
+++ TREM/js/setting.js
@@ -5,39 +5,36 @@ const os = require("node:os");
 let Loc;
 const win = getCurrentWindow();
 
-// When document has loaded, initialise
-document.onreadystatechange = (event) => {
+document.onreadystatechange = () => {
 	if (document.readyState == "complete")
 		handleWindowControls();
 
 };
 
-window.onbeforeunload = (event) => {
-	/* If window is reloaded, remove win event listeners
-    (DOM element listeners get auto garbage collected but not
-    Electron win listeners as the win is not dereferenced unless closed) */
+window.onbeforeunload = async () => {
+	await $(document.body).fadeOut(100).promise();
 	win.removeAllListeners();
+	win.destroy();
 };
 
 function handleWindowControls() {
 	// Make minimise/maximise/restore/close buttons work when they are clicked
-	document.getElementById("min-button").addEventListener("click", event => {
+	document.getElementById("min-button").addEventListener("click", () => {
 		win.minimize();
 	});
 
-	document.getElementById("max-button").addEventListener("click", event => {
+	document.getElementById("max-button").addEventListener("click", () => {
 		win.maximize();
 	});
 
-	document.getElementById("restore-button").addEventListener("click", event => {
+	document.getElementById("restore-button").addEventListener("click", () => {
 		win.unmaximize();
 	});
 
-	document.getElementById("close-button").addEventListener("click", event => {
+	document.getElementById("close-button").addEventListener("click", () => {
 		win.close();
 	});
 
-	// Toggle maximise/restore buttons when maximisation/unmaximisation occurs
 	toggleMaxRestoreButtons();
 	win.on("maximize", toggleMaxRestoreButtons);
 	win.on("unmaximize", toggleMaxRestoreButtons);
